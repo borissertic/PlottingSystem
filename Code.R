d949e834c2658b3bf8928b38df91fe90919fbb9c
borissertic <boris.sertic@gmail.com>
2019-01-17 16:58:24 +0100

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,1 +1,8 @@
+#Import the data
+df <- read.csv(file = "household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
 
+#Converting date variables
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
+
+#Subseting the data
+df_sub <- subset(df, df$Date >= as.Date("2007-02-01") & df$Date <= as.Date("2007-02-02"))
